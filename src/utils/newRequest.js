9a14445bf7821af3defdd1d7a8ef76b04238cab2
VenkateshSircilla123 <113850972+VenkateshSircilla123@users.noreply.github.com>
2023-10-17 21:34:58 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const newRequest = axios.create({baseURL: 'https://vsfiverrapp1.onrender.com/api', withCredentials:true})
+const newRequest = axios.create({baseURL: 'https://vs-fiverr-app-backend-q04oes7hf-venkateshsircilla123s-projects.vercel.app/api', withCredentials:true})
 
 export default newRequest
